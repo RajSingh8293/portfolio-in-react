4e5167ffa5e03ebf63e3f28213ef9d30eedfd6fe
Rajsingh <singhraj@gmail.com>
2024-03-27 22:01:43 +0530

    Add vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
+import commonjs from '@rollup/plugin-commonjs';
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    commonjs({
+      include: /node_modules/ // Only transpile CommonJS modules from node_modules
+    }),
+  ],
 })
+
